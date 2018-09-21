aaaa39ad491c05e0a1f9d5cd4cadc280a869413b
Katrina Aganon <katrina.aganon@gmail.com>
2018-09-20 23:08:39 -0700

    added edit and detail pages, and links to each page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,32 +1,68 @@
-
 class TasksController < ApplicationController
-    def index
-      @tasks = Task.all
+
+
+  def index
+    @tasks = Task.all
+  end
+
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      head :not_found
+    end
+  end
+
+
+  def new
+    @task = Task.new
+  end
+
+
+  def create
+    @task = Task.new(
+      action: params[:task][:action],
+      description: params[:task][:description],
+      completion_date: params[:task][:completion_date]
+    )
+
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  def edit
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      head :not_found
+    end
+  end
+
+
+  def update
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      head :not_found
     end
 
-    def show
-      task_id = params[:id].to_i
-      @task = Task.find_by(id: task_id)
-      if @task.nil?
-        head :not_found
-      end
+    is_updated = @task.update(
+      action: params[:task][:action],
+      description: params[:task][:description],
+      completion_date: params[:task][:completion_date]
+    )
+
+    if is_updated
+      redirect_to task_path(@task)
+    else
+      render :edit
     end
 
-    def new
-      @task = Task.new
-    end
-
-    def create
-      @task = Task.new(action: params[:task][:action], description: params[:task][:description], completion_date: params[:task][:completion_date])
-
-      @task.save 
-
-      if @task.save
-        redirect_to root_path
-      else
-        render :new
-      end
-
-    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,13 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
   post '/tasks', to: 'tasks#create', as: 'tasks'
 
-  get '/tasks', to: 'tasks#index'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
 
+  get '/tasks', to: 'tasks#index'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+
+
 
 end
