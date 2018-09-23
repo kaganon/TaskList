00ab563992f7bdaef390d4eda0336a6ced8222de
Katrina Aganon <katrina.aganon@gmail.com>
2018-09-22 18:37:06 -0700

    added task_params and private methods to controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,7 @@ class TasksController < ApplicationController
 
 
   def show
-    task_id = params[:id].to_i
+    task_id = params[:id]
     @task = Task.find_by(id: task_id)
     if @task.nil?
       head :not_found
@@ -21,11 +21,7 @@ class TasksController < ApplicationController
 
 
   def create
-    @task = Task.new(
-      action: params[:task][:action],
-      description: params[:task][:description],
-      completion_date: params[:task][:completion_date]
-    )
+    @task = Task.new(task_params)
 
     if @task.save
       redirect_to root_path
@@ -36,7 +32,7 @@ class TasksController < ApplicationController
 
 
   def edit
-    task_id = params[:id].to_i
+    task_id = params[:id]
     @task = Task.find_by(id: task_id)
     if @task.nil?
       head :not_found
@@ -45,16 +41,21 @@ class TasksController < ApplicationController
 
 
   def update
-    task_id = params[:id].to_i
+    task_id = params[:id]
     @task = Task.find_by(id: task_id)
+
     if @task.nil?
       head :not_found
     end
 
+    # use params (task.update(book_params))
+    # check if the render path is the correct one?
+
     is_updated = @task.update(
       action: params[:task][:action],
       description: params[:task][:description],
-      completion_date: params[:task][:completion_date]
+      completion_date: params[:task][:completion_date],
+      completed: params[:task][:completed]
     )
 
     if is_updated
@@ -62,7 +63,58 @@ class TasksController < ApplicationController
     else
       render :edit
     end
-
   end
+
+  def destroy
+    task_id = params[:id]
+    task = Task.find_by(id: task_id)
+
+    if task.nil?
+      head :not_found
+    end
+
+    if task.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
+  def complete
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+
+    if @task.nil?
+      head :not_found
+    end
+
+    @task.update_attribute(:completed, true)
+    @task.update_attribute(:completion_date, Date.today)
+
+    redirect_to root_path
+  end
+
+  def incomplete
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+
+    if @task.nil?
+      head :not_found
+    end
+
+    @task.update_attribute(:completed, false)
+    @task.update_attribute(:completion_date, nil)
+
+    redirect_to root_path
+  end
+
+  private
+
+  def task_params
+    return params.require(:task).permit(:action, :description, :completion_date, :completed, false)
+  end
+
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
 
-
-
+  delete 'tasks/:id', to: 'tasks#destroy'
+  patch 'tasks/:id/complete', to: 'tasks#complete', as: 'complete_task'
+  patch 'tasks/:id/incomplete', to: 'tasks#incomplete', as: 'incomplete_task'
 
 end
