f589adf5b428cffaba90dd0276349ff3f41516f6
Katrina Aganon <katrina.aganon@gmail.com>
2018-09-23 23:02:37 -0700

    refactored routes with resources magic shortcut and adjusted  tabs and spaces

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,19 @@ Rails.application.routes.draw do
 
   root 'tasks#index'
 
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post '/tasks', to: 'tasks#create', as: 'tasks'
-
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  patch 'tasks/:id', to: 'tasks#update'
-
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-
-  delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
   
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # post '/tasks', to: 'tasks#create', as: 'tasks'
+  #
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch 'tasks/:id', to: 'tasks#update'
+  #
+  # get '/tasks', to: 'tasks#index'
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  #
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
   patch 'tasks/:id/complete', to: 'tasks#complete', as: 'complete_task'
   patch 'tasks/:id/incomplete', to: 'tasks#incomplete', as: 'incomplete_task'
 
